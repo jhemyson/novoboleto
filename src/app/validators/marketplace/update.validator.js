b85baa3f93a38f8e472c06894c62ece52b021b10
jhemyson <jhemyson.maqplan@hotmail.com>
2019-09-13 16:58:53 -0300

    update validator

---
+++ src/app/validators/marketplace/update.validator.js
@@ -0,0 +1,18 @@
+import yup from 'yup'
+import Result from '../../common/result'
+
+export default async (req, res, next) => {
+  try {
+    const body = yup.object().shape({
+      number: yup.string(),
+      active: yup.boolean()
+    })
+
+    await body.validate(req.body, { abortEarly: false });
+
+    return next()
+  } catch(error){
+    const result = new Result('Erro de validação', false, null, error.inner)
+    return result.unsuccess(res, 400)
+  }
+}
